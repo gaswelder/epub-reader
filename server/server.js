ad6236e17f4360ccc99894168d3c70f5a8c5cdfe
gasweld <gasweld@gmail.com>
2019-05-10 23:43:40 +0300

    Book preview server

---
+++ server/server.js
@@ -0,0 +1,59 @@
+const http = require("http");
+const fs = require("fs");
+const { Book } = require("../book");
+
+function read(name) {
+  return fs.readFileSync("../samples/" + name);
+}
+
+function list() {
+  const ls = fs.readdirSync("../samples");
+  return ls.filter(f => f.endsWith(".epub"));
+}
+
+http
+  .createServer(async function(req, res) {
+    try {
+      switch (req.url) {
+        case "/":
+          await index(req, res);
+          break;
+        default:
+          await cover(req, res);
+      }
+    } catch (e) {
+      res.setHeader("Content-Type", "text/plain");
+      res.writeHead(500);
+      res.write("Error: " + e.toString());
+      res.end();
+    }
+  })
+  .listen(8080);
+
+async function index(req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.write(`<style>
+  body {
+      display: flex;
+      flex-wrap: wrap;
+  }
+  article {
+      width: 200px;
+  }
+  img { height: 200px; width: auto; }
+  </style>`);
+  for (const name of list()) {
+    res.write(`<article><img src="/${name}"><br>${name}</article>`);
+  }
+  res.end();
+}
+
+async function cover(req, res) {
+  const name = unescape(req.url.substr(1));
+  const src = read(name);
+  const book = await Book.load(src);
+  const c = await book.cover();
+  res.setHeader("Content-Type", c.type);
+  res.write(await c.buffer());
+  res.end();
+}
