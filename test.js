0454d57fa94dc4b787bab5744fe1b73726c904ee
gasweld <gasweld@gmail.com>
2019-05-16 00:59:51 +0300

    Tests rewrite

--- test.js
+++ test.js
@@ -3,118 +3,108 @@ const fs = require("fs");
 const { Book } = require("./book");
 const filters = require("./src/filters");
 
-// async function bb() {
-//   const book = await Book.load(path);
-
-//   const html = book.chapters().map(filter).join('');
-// }
-
-async function toc(book) {
-  function TOC(toc, indent = "  ") {
-    let s = "";
-    for (const t of toc) {
-      s += indent + t.title() + ` (${t.href()})` + "\n";
-      if (t.children().length > 0) {
-        s += TOC(t.children(), indent + indent);
-      }
-    }
-    return s;
+const files = new Map();
+function readfile(name) {
+  if (!files.has(name)) {
+    files.set(name, fs.readFileSync(`samples/${name}.epub`));
   }
-
-  const toc = await book.toc();
-  console.log(TOC(toc));
+  return files.get(name);
 }
 
-async function content(book, imagesNumber) {
-  const html = (await book.pager().all()).join("");
-  assert.equal(html.match(/<img /g).length, imagesNumber);
-}
+describe("content check", function() {
+  const samples = [["comp", 28], ["jeff", 12], ["math", 126]];
 
-async function testFilters() {
-  const body = {
-    name: "body",
-    children: [
-      {
-        name: "svg",
-        attr: {
-          height: "100%"
-        },
-        children: [
-          {
-            name: "img",
-            attr: {
-              style: "border: 1px; height: 100%; margin: 1em;"
-            }
-          }
-        ]
-      }
-    ]
-  };
+  for (const [name, imagesNumber] of samples) {
+    it(name, async function() {
+      const src = readfile(name);
+      const book = await Book.load(src);
+      const html = (await book.pager().all()).join("");
+      assert.equal(html.match(/<img /g).length, imagesNumber);
+    });
+  }
+});
 
-  filters.apply(body);
-
-  assert.equal(
-    body.children[0].children[0].attr.style,
-    "border: 1px; margin: 1em;"
-  );
-
-  assert.equal(body.children[0].name, "svg");
-  assert.equal(body.children[0].attr.height, undefined);
-}
-
-async function main() {
-  testFilters();
-
-  await progress("jeff");
-
-  const samples = [
-    ["nov", 893],
-    ["comp", 28],
-    ["jeff", 12],
-    ["go", 767],
-    ["math", 126],
-    ["swim", 260]
-  ];
-
-  for (const sample of samples) {
-    const [name, imagesNumber] = sample;
-    console.log(name);
-    const src = fs.readFileSync(`samples/${name}.epub`);
+describe("epub", function() {
+  it("reads covers", async function() {
+    const src = fs.readFileSync("samples/jeff.epub");
     const book = await Book.load(src);
-    await toc(book);
-    await content(book, imagesNumber);
-  }
-
-  await pages();
-  await covers();
-}
-
-async function progress(name) {
-  const src = fs.readFileSync(`samples/${name}.epub`);
-  const book = await Book.load(src);
-  const pager = book.pager();
-
-  const states = [];
-  pager.onConvertProgress(function(n) {
-    states.push(n);
+    assert.equal((await book.cover()).type, "image/jpeg");
   });
 
-  await pager.all();
-  console.log(states);
-  assert.ok(states.length > 1);
-}
+  it("splits to pages", async function() {
+    const src = fs.readFileSync("samples/jeff.epub");
+    const book = await Book.load(src);
+    const pages = await book.pager().all();
+    assert.ok(pages.length > 1);
+  });
 
-async function pages() {
-  const src = fs.readFileSync("samples/jeff.epub");
-  const book = await Book.load(src);
-  const pages = await book.pages();
-  console.log(pages);
-}
+  it("has progress callback", async function() {
+    const src = fs.readFileSync(`samples/jeff.epub`);
+    const book = await Book.load(src);
+    const pager = book.pager();
 
-async function covers() {
-  const src = fs.readFileSync("samples/jeff.epub");
-  const book = await Book.load(src);
-  assert.equal((await book.cover()).type, "image/jpeg");
-}
+    const progressValues = [];
+    pager.onConvertProgress(function(n) {
+      progressValues.push(n);
+    });
 
-main();
+    await pager.all();
+    assert.ok(progressValues.length > 1);
+  });
+});
+
+describe("toc", function() {
+  async function toc(book) {
+    function TOC(toc, indent = "  ") {
+      let s = "";
+      for (const t of toc) {
+        s += indent + t.title() + ` (${t.href()})` + "\n";
+        if (t.children().length > 0) {
+          s += TOC(t.children(), indent + indent);
+        }
+      }
+      return s;
+    }
+
+    const toc = await book.toc();
+    console.log(TOC(toc));
+  }
+
+  it("toc", async function() {
+    const src = readfile("jeff");
+    const book = await Book.load(src);
+    return toc(book);
+  });
+
+  it("filters", function() {
+    const body = {
+      name: "body",
+      children: [
+        {
+          name: "svg",
+          attr: {
+            height: "100%"
+          },
+          children: [
+            {
+              name: "img",
+              attr: {
+                style: "border: 1px; height: 100%; margin: 1em;"
+              }
+            }
+          ]
+        }
+      ]
+    };
+
+    filters.apply(body);
+
+    assert.equal(
+      body.children[0].children[0].attr.style,
+      "border: 1px; margin: 1em;"
+    );
+
+    assert.equal(body.children[0].name, "svg");
+    assert.equal(body.children[0].attr.height, undefined);
+  });
+});
