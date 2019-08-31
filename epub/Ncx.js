d0fd8f96df602bd69e725dd5934e28149c1ad078
gasweld <gasweld@gmail.com>
2019-09-01 01:42:08 +0300

    Unroll ncx functions

--- epub/Ncx.js
+++ epub/Ncx.js
@@ -5,29 +5,6 @@ module.exports = Ncx;
 
 function Ncx(manifest) {
   this.list = async function() {
-    function ns(data) {
-      if (typeof data != "object") {
-        return data;
-      }
-      return new Proxy(data, {
-        get(t, k) {
-          if (typeof k != "string") {
-            return t[k];
-          }
-          return ns(t[k] || t["ncx:" + k]);
-        }
-      });
-    }
-
-    function parsePoints(root) {
-      return root.map(function(p) {
-        const title = p.navLabel[0].text[0];
-        const src = p.content[0].$.src;
-        const children = p.navPoint ? parsePoints(p.navPoint) : [];
-        return new NavPoint(title, src, children);
-      });
-    }
-
     const xml = await parseXML(
       await manifest
         .node()
@@ -36,6 +13,29 @@ function Ncx(manifest) {
     );
     return parsePoints(ns(xml.ncx.navMap[0]).navPoint);
   };
+}
+
+function parsePoints(root) {
+  return root.map(function(p) {
+    const title = p.navLabel[0].text[0];
+    const src = p.content[0].$.src;
+    const children = p.navPoint ? parsePoints(p.navPoint) : [];
+    return new NavPoint(title, src, children);
+  });
+}
+
+function ns(data) {
+  if (typeof data != "object") {
+    return data;
+  }
+  return new Proxy(data, {
+    get(t, k) {
+      if (typeof k != "string") {
+        return t[k];
+      }
+      return ns(t[k] || t["ncx:" + k]);
+    }
+  });
 }
 
 function parseXML(str, options = {}) {
