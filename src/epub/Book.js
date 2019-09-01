937307f0c7dae6f83e5eb58f0a1866f15fe553b8
gasweld <gasweld@gmail.com>
2019-09-01 21:21:13 +0300

    Moved book init code

--- src/epub/Book.js
+++ src/epub/Book.js
@@ -1,7 +1,3 @@
-const JSZip = require("jszip");
-const xml2js = require("xml2js");
-const Manifest = require("./Manifest");
-const ZipNode = require("./ZipNode");
 const Ncx = require("./Ncx");
 
 module.exports = Book;
@@ -41,53 +37,4 @@ function Book(manifest) {
     }
     return css;
   };
-}
-
-/**
- * Reads the given source and returns a book object.
- * The source is whatever can be read by JSZip.
- */
-Book.load = async function(src) {
-  const zip = await new JSZip().loadAsync(src);
-  const manifest = await getManifest(zip);
-  return new Book(manifest);
-};
-
-/**
- * Reads the given zip and returns a manifest object.
- *
- * @param {JSZip} zip
- * @returns {Manifest}
- */
-async function getManifest(zip) {
-  const indexPath = await rootFilePath(zip);
-  const indexNode = new ZipNode(zip, indexPath);
-  const manifestData = await parseXML(
-    await zip.file(indexPath).async("string")
-  );
-  return new Manifest(indexNode, manifestData);
-}
-
-/**
- * Returns the path to the manifest file.
- */
-async function rootFilePath(zip) {
-  const container = await zip.file("META-INF/container.xml").async("string");
-  const containerData = await parseXML(container);
-  const rootFile = containerData.container.rootfiles[0].rootfile[0];
-  if (rootFile.$["media-type"] != "application/oebps-package+xml") {
-    throw new Error(
-      "Expected 'application/oebps-package+xml, got " + rootFile.$["media-type"]
-    );
-  }
-  return rootFile.$["full-path"];
-}
-
-function parseXML(str, options = {}) {
-  return new Promise(function(ok) {
-    xml2js.parseString(str, options, function(err, data) {
-      if (err) throw err;
-      ok(data);
-    });
-  });
 }
--- src/epub/test.js
+++ src/epub/test.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const fs = require("fs");
-const Book = require("./Book.js");
+const Book = require(".");
 
 init();
 async function init() {
