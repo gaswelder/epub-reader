26b40b07c9554539165123ee06ddf910d9e23e97
gasweld <gasweld@gmail.com>
2020-10-14 23:47:01 +0300

    Render toc as markup

--- src/viewer/components/sidebar.svelte
+++ src/viewer/components/sidebar.svelte
@@ -4,50 +4,38 @@
   export let bookProxy;
 
   let open = false;
+  let chapters = [];
 
   const initSidebar = function() {
     const menu = document.querySelector("#menu");
-    const toc = document.querySelector("#toc");
     const file = document.querySelector("#file");
-
-    const tocComponent = new Toc({
-      target: toc,
-      props: { chapters: [] }
-    });
-
-    /**
-     * Hide the menu when a chapter is selected from it.
-     */
-    toc.addEventListener("click", function(event) {
-      if (event.target.tagName.toLowerCase() == "a") {
-        closeMenu();
-      }
-    });
 
     /**
      * Hide the menu when a new book file is chosen.
      */
     file.addEventListener("change", function() {
-      closeMenu();
+      open = false;
     });
 
     /**
      * Refresh the table of contents when the book is loaded.
      */
     bookProxy.onChange(async () => {
-      tocComponent.$set({ chapters: await bookProxy.get().toc() });
+      chapters = await bookProxy.get().toc();
     });
-
-    function closeMenu() {
-      open = false;
-    }
   };
   onMount(initSidebar);
 </script>
 
 <div id="menu" class:open>
   <div>
-    <div id="toc" />
+    <div id="toc">
+      <Toc
+        on:chapterclick={() => {
+          open = false;
+        }}
+        {chapters} />
+    </div>
   </div>
   <button
     data-toggle
--- src/viewer/components/toc.svelte
+++ src/viewer/components/toc.svelte
@@ -1,13 +1,22 @@
 <script>
+  import { createEventDispatcher } from "svelte";
   export let chapters;
+
+  const dispatch = createEventDispatcher();
 </script>
 
 {#if chapters}
   <ol>
     {#each chapters as c}
       <li>
-        <a href="#{c.path()}">{c.title()}</a>
-        <svelte:self chapters={c.children()} />
+        <a
+          href="#{c.path()}"
+          on:click={e => {
+            dispatch('chapterclick', { chapter: c });
+          }}>
+          {c.title()}
+        </a>
+        <svelte:self on:chapterclick chapters={c.children()} />
       </li>
     {/each}
   </ol>
