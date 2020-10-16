92de33ad5739b1698e7cbeb4a1d058912e380967
gasweld <gasweld@gmail.com>
2020-10-17 00:18:00 +0300

    Move styles

--- src/viewer/components/toc.svelte
+++ src/viewer/components/toc.svelte
@@ -5,6 +5,23 @@
   const dispatch = createEventDispatcher();
 </script>
 
+<style>
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  li {
+    margin: 1em 0;
+    color: #d8def0;
+    font-family: sans-serif;
+  }
+  ol {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
 {#if chapters}
   <ol>
     {#each chapters as c}
